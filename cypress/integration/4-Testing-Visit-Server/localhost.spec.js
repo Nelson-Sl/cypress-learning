117958027425904e557684fb6d06d8b3d58b8094
shen.liao <shen.liao@thoughtworks.com>
2021-08-27 16:56:34 +0800

    feat: use baseUrl configuration to change default page visited

--- cypress/integration/4-Testing-Visit-Server/localhost.spec.js
+++ cypress/integration/4-Testing-Visit-Server/localhost.spec.js
@@ -1,7 +1,7 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
-        cy.visit('http://localhost:8085') // change URL to match your dev URL
+        cy.visit('/') // change URL to match your dev URL
         cy.contains('Hello World Coming from 1');
-        cy.contains('Hello World Coming from 2t');
+        cy.contains('Hello World Coming from 2');
     })
 });
